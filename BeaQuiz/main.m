cd280bc56bd94255e9dfaead59caa12966c4e41f
Pascal CAMARA <pascalmail77@gmail.com>
2016-11-29 09:42:11 +0100

    Initial Commit

---
+++ BeaQuiz/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  BeaQuiz
+//
+//  Created by Pascal CAMARA on 29/11/2016.
+//  Copyright © 2016 Pascal CAMARA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
